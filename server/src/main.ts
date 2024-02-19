629d215461cb7b426b9292053eb0a69f4e51ed39
S Ray <selmen.rayen.92@gmail.com>
2024-02-19 15:38:00 +0100

    Add auth resource

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Only allows properties explicitly defined in DTO
+      forbidNonWhitelisted: true, // Rejects any property not explicitly allowed
+      transform: true, // Automatically transforms data based on decorators
+    }),
+  );
+  await app.listen(5000);
 }
 bootstrap();
